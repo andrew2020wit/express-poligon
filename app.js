caf274636fb475b7c36dd84686b1e082cda0dafb
andrew2020wit <andrew2020wit@gmail.com>
2020-11-20 19:51:52 +0200

    upload: filter, limit, storage

--- app.js
+++ app.js
@@ -2,7 +2,33 @@ const express = require("express");
 var exphbs = require("express-handlebars");
 
 var multer = require("multer");
-var upload = multer({ dest: "uploads/" });
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+var upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 10000000 },
+});
 
 const port = 3000;
 const app = express();
