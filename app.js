0a61619ccd22cc792f664d4bd1672bb2a6c1688c
andrew2020wit <andrew2020wit@gmail.com>
2020-11-20 19:30:26 +0200

    simple upload

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const express = require("express");
 var exphbs = require("express-handlebars");
 
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+
 const port = 3000;
 const app = express();
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+// Static Server
 app.use(express.static(__dirname + "/public"));
 
+// ============== Upload
+app.post("/upload", upload.single("filedata"), function (req, res, next) {
+  // req.file - info about file
+  console.log("req.file:", req.file);
+
+  console.log("req.body:", req.body);
+  res.render("file-upload");
+});
+
+// ========== Start Standard pages
 app.get("/", function (req, res) {
   res.render("home");
+});
+
+app.get("/file-upload", function (req, res) {
+  res.render("file-upload");
 });
 
 app.get("/about", function (req, res) {
@@ -20,6 +38,7 @@ app.get("/about", function (req, res) {
 app.use(function (req, res) {
   res.send("Not Found");
 });
+// ========== End Standard pages
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
